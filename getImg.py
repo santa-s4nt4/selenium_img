f768e3506d1e355857532854f95566129f132eed
santa-sukitoku <santanaruse@gmail.com>
2019-09-12 23:41:40 +0900

    森七菜のInstagramアカウントの投稿のサムネイルを保存するコード

--- getImg.py
+++ getImg.py
@@ -4,10 +4,11 @@ import io
 from urllib import request
 
 browser = webdriver.Chrome(executable_path='C:\\Users\\santa\\chromedriver_win32\\chromedriver.exe')
-browser.get('https://santa-sukitoku.github.io/santa-portfolio/')
-elem = browser.find_element_by_class_name('module')
-elem = elem.find_element_by_tag_name('img')
-url = elem.get_attribute('src')
-f = io.BytesIO(request.urlopen(url).read())
-img = Image.open(f)
-img.save('./img/img01.jpg')
+browser.get('https://www.instagram.com/morinana_official/')
+elems = browser.find_elements_by_class_name('KL4Bh')
+for index, elem in enumerate(elems):
+        elem = elem.find_element_by_tag_name('img')
+        url = elem.get_attribute('src')
+        f = io.BytesIO(request.urlopen(url).read())
+        img = Image.open(f)
+        img.save('./img/img{}.jpg'.format(index))
